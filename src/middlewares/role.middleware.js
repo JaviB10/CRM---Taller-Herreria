6725d0ff0dcef89deb30a2777f6005f5bf4b6288
Javier Ballón <javi4195@gmail.com>
2024-10-31 09:14:06 -0300

    Eliminacion de middlewares sin uso

--- src/middlewares/role.middleware.js
+++
@@ -1,9 +0,0 @@
-export const roleMiddleware = (requiredRoles) => {
-    return (req, res, next) => {
-        const { user } = req;
-        if (!user || !requiredRoles.includes(user.role)) {
-            return res.status(403).json({ message: 'Forbidden: insufficient permissions' });
-        }
-        next();
-    };
-};
